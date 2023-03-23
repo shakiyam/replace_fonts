27213b0b1cf2948b733673be24205eb64376309c
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2023-03-23 14:57:17 +0900

    Fix Windows encoding problem

--- test/run.bat
+++ test/run.bat
@@ -2,5 +2,5 @@
 pushd "%~dp0"
 call clean.bat
 copy original\sample?.pptx .
-for %%f in (sample?.pptx) do python ..\replace_fonts.py %%f
+for %%f in (sample?.pptx) do python -Xutf8 ..\replace_fonts.py %%f
 popd
