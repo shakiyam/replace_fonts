81a219fcf8cb793ba8aa93ec4d9af504373240f2
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2023-03-23 14:58:08 +0900

    Use wildcard

--- test/run_dev.sh
+++ test/run_dev.sh
@@ -4,4 +4,4 @@ set -eu -o pipefail
 cd "$(dirname "$0")"
 ./clean.sh
 cp original/sample?.pptx .
-../replace_fonts_dev python3 /replace_fonts.py sample1.pptx sample2.pptx sample3.pptx
+../replace_fonts_dev python3 /replace_fonts.py sample?.pptx
