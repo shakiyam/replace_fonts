df25f033d2b12c4590d979379e8b76fab97c02b2
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2024-06-03 03:30:30 +0000

    Bump python to 3.12

--- test/run_dev.sh
+++ test/run_dev.sh
@@ -4,4 +4,4 @@ set -eu -o pipefail
 cd "$(dirname "$0")"
 ./clean.sh
 cp original/sample?.pptx .
-../replace_fonts_dev python3.11 /replace_fonts.py --code sample?.pptx
+../replace_fonts_dev python3.12 /replace_fonts.py --code sample?.pptx
